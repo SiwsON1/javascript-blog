cfd99fd104c724e0abd8a2d497a3d71f55a94704
Marcin Siwonia <mahinek12@gmail.com>
2022-12-11 14:35:50 +0100

    Add final js for titleClickHandler function

--- js/script.js
+++ js/script.js
@@ -5,9 +5,11 @@
   });
   */
   const titleClickHandler = function(event){
+    event.preventDefault();
     const clickedElement = this;
     console.log('Link was clicked!');
     console.log(event);
+    
     /* [DONE] remove class 'active' from all article links  */
 
     const activeLinks = document.querySelectorAll('.titles a.active');
@@ -15,7 +17,7 @@
     for(let activeLink of activeLinks){
     activeLink.classList.remove('active');
     }
-    /* [IN PROGRESS] add class 'active' to the clicked link */
+    /* [DONE] add class 'active' to the clicked link */
     console.log('clickedElement:', clickedElement);
     clickedElement.classList.add('active');
 
@@ -26,12 +28,15 @@
     activeArticle.classList.remove('active');
     }
 
-    /* get 'href' attribute from the clicked link */
-
+    /* [DONE] get 'href' attribute from the clicked link */
+      const articleSelector = clickedElement.getAttribute('href');
+      console.log(articleSelector);
+      
     /* find the correct article using the selector (value of 'href' attribute) */
-
+      const targetArticle = document.querySelector(articleSelector);
+      console.log(targetArticle);
     /* add class 'active' to the correct article */
-
+      targetArticle.classList.add('active');
   }
   
   const links = document.querySelectorAll('.titles a');
